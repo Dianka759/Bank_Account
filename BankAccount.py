a1287f3343e9acb2c05ee1c01a4940d619950343
dianka759 <dianka759@gmail.com>
2021-10-24 17:27:50 -0400

    added @classmethod

--- BankAccount.py
+++ BankAccount.py
@@ -1,7 +1,11 @@
 class BankAccount:
-    def __init__(self, int_rate, balance=0):
+    accounts = []
+
+    def __init__(self, name, int_rate, balance=0):
+        self.name = name
         self.int_rate = int_rate
         self.balance = balance
+        BankAccount.accounts.append(self)
 
     def deposit(self, amount):
         self.balance += amount
@@ -17,25 +21,33 @@ class BankAccount:
             return self
 
     def display_account_info(self):
-        print("Balance:", self.balance)
+        print(f"Account: {self.name}, Balance: {self.balance}")
         return self
 
     def yield_interest(self):
         if self.balance > 0:
             self.balance += self.balance * self.int_rate
         else:
-            print("No interest because balance is negative")
+            print("No interest acquired because balance is negative")
         return self
 
-    # use a classmethod to print all instances of a Bank Account's info
-    # @classmethod
+    @classmethod
+    def print_all_accounts(cls):
+        print("All instances of a Bank Account's info:")
+        for account in cls.accounts:
+            account.display_account_info()
 
 
-normal_people = BankAccount(.03, 100)
-me = BankAccount(.15)
+# declaring two bank accounts
+normal_people = BankAccount("Normal People",.03, 100)
+me = BankAccount("Me",.15)
 
+# making deposits, withdrawls, interest, and displaying ending balance
 normal_people.deposit(1000).deposit(10).deposit(
     20).withdraw(15).yield_interest().display_account_info()
 print()   # for visual purposes, to separate the two account balances
 me.deposit(100).deposit(10).withdraw(100).withdraw(5).withdraw(
     5).withdraw(20).yield_interest().display_account_info()
+
+print() # again, for visual purposes, to separate from the rest
+BankAccount.print_all_accounts()
